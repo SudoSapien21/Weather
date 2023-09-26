9d200e5f9915b3644a790096d9d33900702e0187
Clarence Tolson <sudosapien21@gmail.com>
2023-09-25 23:32:10 -0500

    updated script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,53 @@
-var apiKey = '47150266341be89cf81b739187a460e5';
+var apiKey = "1532b8b61e80f8271018c25a99baba89";
 
 // Check if API key exists in localStorage
-var storedApiKey = localStorage.getItem('apiKey');
-if (storedApiKey) {
-  apiKey = storedApiKey;
-} else {
-  // Prompt the user to enter the API key and store it in localStorage
-  var userApiKey = prompt('Please enter your API key');
-  localStorage.setItem('apiKey', userApiKey);
-  apiKey = userApiKey;
-}
+// var storedApiKey = localStorage.getItem('apiKey');
+// if (storedApiKey) {
+//   apiKey = storedApiKey;
+// } else {
+//   // Prompt the user to enter the API key and store it in localStorage
+//   var userApiKey = prompt('Please enter your API key');
+//   localStorage.setItem('apiKey', userApiKey);
+//   apiKey = userApiKey;
+// }
 
 
-  async function fetchWeatherData(city) {
-    var locationUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  
-    try {
-      var locationResponse = await fetch(locationUrl);
-      if (!locationResponse.ok) {
-        console.error('City not Found!');
-        return;
-      }
-      var locationData = await locationResponse.json();
-      var { lat, lon } = locationData.coord;
+async function fetchWeatherData(city) {
+ 
+  var locationUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
- // Fetch weather data using latitude and longitude
- var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
- var response = await fetch(weatherUrl);
- if (!response.ok) {
-   throw new Error('Weather data not found.');
- }
- var data = await response.json();
- return data;
-} catch (error) {
- console.error('Error:', error);
+  try {
+    var locationResponse = await fetch(locationUrl);
+    if (!locationResponse.ok) {
+      throw new Error('City not found.');
+    }
+    var locationData = await locationResponse.json();
+    var { lat, lon } = locationData.coord;
+
+    // Fetch weather data using latitude and longitude
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    var response = await fetch(weatherUrl);
+    if (!response.ok) {
+      throw new Error('Weather data not found.');
+    }
+    var data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
 // Function to display current weather for a city
 function displayCurrentWeather(weather) {
   var { name } = weather.city;
-  var { dt, temp, humidity, wind_speed } = weather.list[0].main;
+  var { dt, temp, humidity } = weather.list[0].main;
+  var { speed } = weather.list[0].wind; 
   var { icon } = weather.list[0].weather[0];
 
   document.getElementById('city-name').textContent = name;
   document.getElementById('current-date').textContent = formatDate(dt);
   document.getElementById('current-temp').textContent = convertKelvinToCelsius(temp);
   document.getElementById('current-humidity').textContent = humidity;
-  document.getElementById('current-wind-speed').textContent = wind_speed;
+  document.getElementById('current-wind-speed').textContent = speed + ' m/s'; 
   document.getElementById('weather-icon').setAttribute('src', `http://openweathermap.org/img/w/${icon}.png`);
 }
 
@@ -74,10 +75,11 @@ function displayForecast(weather) {
   }
 }
 
+
 // Function to format the date
 function formatDate(timestamp) {
-  var date = new Date(timestamp * 1000);
-  var options = { month: 'short', day: 'numeric' };
+  var date = new Date(timestamp);
+  var options = { month: 'numeric', day: 'numeric', year: 'numeric' };
   return date.toLocaleDateString('en-US', options);
 }
 
@@ -104,8 +106,8 @@ async function handleFormSubmit(event, city) {
       // Add city to search history
       var searchHistory = document.getElementById('search-history');
       var searchItem = document.createElement('li');
-      searchItem.textContent = city;
-      searchItem.addEventListener('click', () => {      
+      // searchItem.textContent = city;
+      searchItem.addEventListener('click', () => {
         handleFormSubmit(event, city);
       });
       searchHistory.prepend(searchItem);
